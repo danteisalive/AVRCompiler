1c7d895108207c66c2f109d008f36f7dbdf3a924
Rasool <arasool.sh2000@gmail.com>
2019-02-24 17:43:40 -0500

    Before messing around

--- PA2/PA2Examples/PA5Cylon.java
+++ PA2/PA2Examples/PA5Cylon.java
@@ -1,7 +1,7 @@
 /**
  * PA5Cylon.java
- * 
- * Canonical example for PA4, updated for PA5.  
+ *
+ * Canonical example for PA4, updated for PA5.
  * Displays a red light that moves
  * back and forth across the LED screen and moves to a row
  * based on key combinations.  The real trick is to make the light
@@ -10,14 +10,14 @@
  * in Meggy.java).
  * In PA6, MeggyJava will be extended to include local variables
  * so that things are even easier than with just function calls.
- * Can keep track of the currentRow and can use a while loop to 
+ * Can keep track of the currentRow and can use a while loop to
  * iterate over the pixels in a row.
- * 
+ *
  *  button combo    row
  *   up              7
  *   up and right    4
  *   down            0
- * 
+ *
  * New language features tested since PA4:
  *  -local variables
  *  -assignment statements
@@ -44,7 +44,7 @@ class CylonPA5 {
     public void run() {
         byte currentRow;
         currentRow=(byte)0;
-        
+
         // Initialize the red LED location to row 0
         Meggy.setPixel( (byte)((byte)0*(byte)0+0-0), currentRow, Meggy.Color.RED );
 
@@ -65,7 +65,7 @@ class CylonPA5 {
             } else {
                 // up and right indicates row 4
                 if (Meggy.checkButton(Meggy.Button.Up)
-                        && Meggy.checkButton(Meggy.Button.Right)) 
+                        && Meggy.checkButton(Meggy.Button.Right))
                 {
                     Meggy.setPixel((byte)0, (byte)0, Meggy.Color.DARK);
                     Meggy.setPixel((byte)0, (byte)7, Meggy.Color.DARK);
@@ -73,7 +73,7 @@ class CylonPA5 {
                     this.cylonRow((byte)0, currentRow);
                 } else {
                     // down indicates row 0
-                    if (Meggy.checkButton(Meggy.Button.Down)) 
+                    if (Meggy.checkButton(Meggy.Button.Down))
                     {
                         Meggy.setPixel((byte)0, (byte)7, Meggy.Color.DARK);
                         Meggy.setPixel((byte)0, (byte)4, Meggy.Color.DARK);
@@ -81,15 +81,15 @@ class CylonPA5 {
                         this.cylonRow((byte)0, currentRow);
                     } else {
                         // Otherwise continue pattern in current row.
-                        this.cylonRow((byte)0, currentRow);                        
+                        this.cylonRow((byte)0, currentRow);
                     }
                 }
             }
             Meggy.delay(1000);
-        } // end while        
+        } // end while
     }
 
-    // Recursive routine that will cycle forward and 
+    // Recursive routine that will cycle forward and
     // back through all columns in the given row starting
     // at col.
     // Assuming that the given column is within range.
@@ -98,7 +98,7 @@ class CylonPA5 {
         i = col;
         // loop forward through all cols
         while (i<(byte)8) {
-            Meggy.setPixel(i, row, Meggy.Color.RED);            
+            Meggy.setPixel(i, row, Meggy.Color.RED);
             // if previous column is within range, then turn off
             if ((byte)(0-1)<(byte)(i-(byte)1)) {
                 Meggy.setPixel((byte)(i-(byte)1), row, Meggy.Color.DARK);
@@ -106,11 +106,11 @@ class CylonPA5 {
             Meggy.delay(256);
             i = (byte)(i+(byte)1);
         }
-        
+
         // loop backward through all cols, but stop at initial col
         i = (byte)7;
         while ((byte)(0-1)<i && col<(byte)(i+(byte)1)) {
-            Meggy.setPixel(i, row, Meggy.Color.RED);            
+            Meggy.setPixel(i, row, Meggy.Color.RED);
             // if previous column is within range, then turn off
             if ((byte)(i+(byte)1)<(byte)8) {
                 Meggy.setPixel((byte)(i+(byte)1), row, Meggy.Color.DARK);
@@ -118,6 +118,6 @@ class CylonPA5 {
             Meggy.delay(256);
             i = (byte)(i-(byte)1);
         }
-        
+
     }
 }
