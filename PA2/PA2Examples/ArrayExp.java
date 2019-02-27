ea1e165f6ab1c195c7f04e8e8196ade780d7a1c3
Rasool <arasool.sh2000@gmail.com>
2019-02-26 20:13:17 -0500

    Array Handling is almost complete

--- PA2/PA2Examples/ArrayExp.java
+++ PA2/PA2Examples/ArrayExp.java
@@ -2,20 +2,20 @@
 
 import meggy.Meggy;
 
-class ArrayExp {     
-    public static void main(String[] a){         
-        Meggy.setPixel((byte)(new MyClass().testing()), (byte)4, Meggy.Color.GREEN);     
-    } 
-} 
+class ArrayExp {
+    public static void main(String[] a){
+        Meggy.setPixel((byte)(new MyClass().testing()), (byte)4, Meggy.Color.GREEN);
+    }
+}
 
 
-class MyClass {     
-    public int testing() {         
-        int [] x;         
-        x = new int [7];         
-        x[0] = 1;         
-        x[1] = 6;         
-        x[6] = 3;         
-        return x[0] + x[x[1]];     
-    } 
+class MyClass {
+    public int testing() {
+        int [] x;
+        x = new int [7];
+        x[0] = 1;
+        x[1] = 6;
+        x[6] = 3;
+        return x[0] + x[1];     
+    }
 }
